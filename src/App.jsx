66e9678ffd3c5f91f40f9e3d9036ef333dee900b
Gonzalo <34680110ax@gmail.com>
2024-08-31 16:47:55 -0300

    config inicial talwind react router dom

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
+import { Navbar } from "./componentes/Navbar"
+
+
+function App() {
+
+
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/projects" element={<h1>Projects</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+
+        </Routes>
+      </Router>
+
+    </main>
+  )
+}
+
+export default App
