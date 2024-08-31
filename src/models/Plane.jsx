ed2275491ca4af9ba3a081f3dfdddc6d25482025
Gonzalo <34680110ax@gmail.com>
2024-08-31 20:43:17 -0300

    añadiendo plane, bird, sky

---
+++ src/models/Plane.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+import planeScene from '../assets/3d/plane.glb'
+
+
+export const Plane = () => {
+  const { scene, animations } = useGLTF(planeScene)
+  return (
+    <mesh>
+      <primitive object={scene} />
+    </mesh>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,9 @@ import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Loader } from '../componentes/Loader'
 import Island from '../models/Island'
-import { Point } from '@react-three/drei'
+import { Plane} from '../models/Plane'
 import { Sky } from '../models/Sky'
+import { Bird } from '../models/Bird'
 
 {/*   <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center' >
         ASDASD
@@ -39,13 +40,14 @@ export const Home = () => {
           <directionalLight position={[1,1,1]} intensity={2} />
           <hemisphereLight skyColor="#b1e1ff" groundColor={"#000000" } intensity={1} />
 
-
+          <Bird />
           <Sky />
           <Island
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
           />
+          <Plane />
 
         </Suspense>
 
