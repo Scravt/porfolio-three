75cda8abf2b0c8fdfdcb59264f3c9b94fbe6c5d6
Gonzalo <34680110ax@gmail.com>
2024-09-15 23:18:36 -0300

    fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { skills } from '../constants/index'
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { skills, experiences } from '../constants/index'
 
 export const About = () => {
   return (
@@ -20,7 +21,7 @@ export const About = () => {
 
           {skills.map((skill, index) => (
             <div key={index} className='block-container w-20 h-20'>
-              <div className='btn-back rounded-xl '/>
+              <div className='btn-back rounded-xl ' />
               <div className='btn-front rounded-xl flex flex-col justify-center items-center'>
                 <img src={skill.imageUrl} alt={skill.name} className='w-1/2 h-1/2 object-contain' />
                 <p className='text-slate-500'>{skill.name}</p>
@@ -32,7 +33,40 @@ export const About = () => {
       </div>
 
       <div className='p-16'>
-        <h3>Work speriencie</h3>
+        <h3>Work Experience</h3>
+        <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+          <p>I've  worked as a freelance developer for the past 2 years. I've built web applications for clients using React, Node.js, and MongoDB. I've also worked on personal projects to improve my skills and learn new technologies. I'm always looking for new opportunities to work on exciting projects and collaborate with other developers.
+          </p>
+        </div>
+        <div className='mt-12 flex'>
+          <VerticalTimeline>
+            {experiences.map((experience, index) => (
+              <VerticalTimelineElement>
+                <div>
+                  <h3 className='text-black text-xl font-semibold '>
+                    {experience.title}
+                  </h3>
+                  <p className='text-black-500 font-medium font-base'
+                    style={{
+                      margin: 0
+                    }}>
+                    {experience.company_name}
+                  </p>
+                  <ul className='my-5 list-disc space-y-2'>
+                    {experience.points.map((item, index) => (
+                      <li key={index} className='text-black-500/50 font-normal pl-1 text-sm'>
+                        {item}
+                      </li>
+                    ))}
+
+                  </ul>
+
+                </div>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+
+        </div>
 
       </div>
 
